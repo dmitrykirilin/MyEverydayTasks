aa759d8332c809cba7913708fe638f879fc6f6b4
Дмитрий Кирилин <Demonus18@gmail.com>
2020-04-25 03:06:51 +0300

    Создан интерфейс и модель данных.

--- MyDiary/MainWindow.xaml.cs
+++ MyDiary/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MyDiary.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,23 @@ namespace MyDiary
     /// </summary>
     public partial class MainWindow : Window
     {
+        public BindingList<Diary> diaryData;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            diaryData = new BindingList<Diary>()
+            {
+                new Diary(){Task = "jkhgkljg"},
+                new Diary(){Task = "jkhgkl3453 p9038",
+                            IsDone = true}
+            };
+
+            MyDataGrid.ItemsSource = diaryData;
         }
     }
 }
---
+++ MyDiary/Model/Diary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDiary.Model
+{
+    public class Diary
+    {
+        public DateTime Creation { get; set; } = DateTime.Now;
+
+        private string task;
+
+        public string Task
+        {
+            get { return task; }
+            set { task = value; }
+        }
+
+        private bool isDone;
+
+        public bool IsDone
+        {
+            get { return isDone; }
+            set { isDone = value; }
+        }
+
+    }
+}
